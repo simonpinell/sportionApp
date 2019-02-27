26b5007e03ae5e5496eb0a5b7c365507dc715074
Simon Pinell <simonpinell@gmail.com>
2019-02-27 13:31:53 +0100

    cypress installation

---
+++ cypress/integration/tabs.spec.ts
@@ -0,0 +1,5 @@
+describe('My First Test', function () {
+    it('Visits sportionApp', function () {
+        cy.visit('https://example.cypress.io')
+    })
+})
--- src/app/pages/tabs/tabs.page.spec.ts
+++ src/app/pages/tabs/tabs.page.spec.ts
@@ -23,4 +23,5 @@ describe('TabsPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
